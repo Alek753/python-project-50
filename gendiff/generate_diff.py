ed9ecb40159288763dbf2d8a73b36a07c6b24250
Aleksei Semenov <alek753@yandex.ru>
2023-09-11 20:41:50 +0400

    some changes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,5 +5,23 @@ def generate_diff(file1, file2):
     print(file1)
     data1 = json.load(open(file1))
     data2 = json.load(open(file2))
+    all_keys = sorted(data1.keys() | data2.keys())
+    same_keys = sorted(data1.keys() & data2.keys())
+    minus = sorted(data1.keys() - data2.keys())
+    plus = sorted(data2.keys() - data1.keys())
+    result = {}
+    for key in all_keys:
+        if key in same_keys:
+            result.update({f'   {key}':data1[key]})
+        elif key in minus:
+            result.update({f' - {key}':data1[key]})
+        elif key in plus:
+            result.update({f' - {key}':data2[key]})
     print(data1)
     print(data2)
+    print(result)
+    out_file = open('gendiff/out.txt', 'w')
+    print(json.dumps(result, sort_keys=True, indent=4))
+#    file1.close()
+#    file2.close()
+    out_file.close()
