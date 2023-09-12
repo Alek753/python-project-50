b44eead10744e65b89fd3d0af1122ce37316f792
Aleksei Semenov <alek753@yandex.ru>
2023-09-12 14:37:50 +0400

    compleeted step 3

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,8 +3,10 @@ import json
 
 def generate_diff(file1, file2):
     print(file1)
-    data1 = json.load(open(file1))
-    data2 = json.load(open(file2))
+    in_file1 = open(file1, "r")
+    in_file2 = open(file2, "r")
+    data1 = json.load(in_file1)
+    data2 = json.load(in_file2)
     all_keys = sorted(data1.keys() | data2.keys())
     same_keys = sorted(data1.keys() & data2.keys())
     minus = sorted(data1.keys() - data2.keys())
@@ -12,16 +14,20 @@ def generate_diff(file1, file2):
     result = {}
     for key in all_keys:
         if key in same_keys:
-            result.update({f'   {key}':data1[key]})
+            if data1[key] == data2[key]:
+                result.update({f'  {key}': data1[key]})
+            else:
+                result.update({f'- {key}': data1[key]})
+                result.update({f'+ {key}': data2[key]})
         elif key in minus:
-            result.update({f' - {key}':data1[key]})
+            result.update({f'- {key}': data1[key]})
         elif key in plus:
-            result.update({f' - {key}':data2[key]})
-    print(data1)
-    print(data2)
-    print(result)
-    out_file = open('gendiff/out.txt', 'w')
-    print(json.dumps(result, sort_keys=True, indent=4))
-#    file1.close()
-#    file2.close()
-    out_file.close()
+            result.update({f'+ {key}': data2[key]})
+#    print(data1)
+#    print(data2)
+#    print(result)
+#    out_file = open('gendiff/out.txt', 'w')
+    in_file1.close()
+    in_file2.close()
+#    out_file.close()
+    return json.dumps(result, indent=2, separators=('', ': ')).replace('"', '')
