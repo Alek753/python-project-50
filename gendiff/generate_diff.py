7f363ae503c2b7e7533a1f36bbaefb6ad64c2003
Aleksei Semenov <alek753@yandex.ru>
2023-09-26 20:56:53 +0400

    Making step 6

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,7 +2,7 @@ from gendiff.parsing import parsing
 from gendiff.make_diff import make_diff
 
 
-def generate_diff(file1, file2):
+def generate_diff(file1, file2, formatter='stylish'):
     data1 = parsing(file1)
     print(data1)
     data2 = parsing(file2)
