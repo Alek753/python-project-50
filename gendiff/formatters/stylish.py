01c943204116c9357205f103ee06c9490ecd0385
Aleksei Semenov <alek753@yandex.ru>
2024-01-31 16:15:14 +0400

    making stylish

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -2,7 +2,6 @@ import itertools
 REPLACER = ' '
 
 
-
 def make_string(current_data, depth):
     if not isinstance(current_data, dict):
         if isinstance(current_data, bool):
@@ -10,25 +9,23 @@ def make_string(current_data, depth):
         if current_data is None:
             return 'null'
         return str(current_data)
-    current_indent = REPLACER * (depth * 4 - 2)
+    current_indent = REPLACER * (depth * 4)
     out_data = []
     for key, value in current_data.items():
         out_data.append(f'{current_indent}{key}: {make_string(value, depth + 1)}')
-    return '\n'.join(itertools.chain("{", out_data, [REPLACER * depth + "}"]))
+    return '\n'.join(itertools.chain("{", out_data, [REPLACER * (depth - 1) * 4 + "}"]))
 
 
 def form_data(indent, data, key, depth):
-    print(indent)
-    print(data['key'])
     return f"{indent}{data['key']}: " \
-           f"{make_string(data[key], depth)}"
+           f"{make_string(data[key], depth + 1)}"
 
 
 def inner(data, depth=1):
     if not isinstance(data, dict):
         return str(data)
     deep_indent = REPLACER * (depth * 4 - 2)
-    indent = REPLACER * depth
+    indent = REPLACER * (depth - 1) * 4
     lines = []
 
 #    print('deep_indent_size', deep_indent_size)
@@ -60,7 +57,7 @@ def inner(data, depth=1):
             lines.append(f"{deep_indent}  {value['key']}: "
                          f"{inner(value['value'], depth + 1)}")
             continue
-    result = itertools.chain("{", lines, [indent * depth + "}"])
+    result = itertools.chain("{", lines, [indent + "}"])
     return "\n".join(result)
 
 
--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -8,15 +8,20 @@ FIXTURES_PATH = f'{os.path.dirname(__file__)}/fixtures'
 
 @pytest.mark.parametrize("file1, file2, expected_filename", [
     (
-        f'{FIXTURES_PATH}/file1.json',
-        f'{FIXTURES_PATH}/file2.json',
+        f'{FIXTURES_PATH}/example_file1.json',
+        f'{FIXTURES_PATH}/example_file2.json',
         f'{FIXTURES_PATH}/expected_for_examples.txt'
     ),
     (
-        f'{FIXTURES_PATH}/file1.yml',
-        f'{FIXTURES_PATH}/file2.yml',
+        f'{FIXTURES_PATH}/example_file1.yml',
+        f'{FIXTURES_PATH}/example_file2.yml',
         f'{FIXTURES_PATH}/expected_for_examples.txt'
-    ), ])
+    ),
+    (
+        f'{FIXTURES_PATH}/file1.json',
+        f'{FIXTURES_PATH}/file2.json',
+        f'{FIXTURES_PATH}/expected_for_stylish.txt'
+    ),])
 def test_generate_diff(file1, file2, expected_filename):
     with open(expected_filename, "r") as expected_file:
         assert expected_file.read() == generate_diff(file1, file2)
