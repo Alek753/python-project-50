86f615db3696d1e96f38ae854ea823103cb00949
Aleksei Semenov <alek753@yandex.ru>
2024-02-01 16:12:41 +0400

    adding plain-format plug

--- gendiff/formatters/choose_formatter.py
+++ gendiff/formatters/choose_formatter.py
@@ -1,6 +1,9 @@
 from gendiff.formatters.stylish import make_stylish
+from gendiff.formatters.plain import make_plain
 
 
 def choose_formatter(diff, formatter):
     if formatter == 'stylish':
         return make_stylish(diff)
+    if formatter == 'plain':
+        return make_plain(diff)
---
+++ gendiff/formatters/plain.py
@@ -0,0 +1,7 @@
+def inner(data):
+    result = []
+    return result
+
+
+def make_plain(diff):
+    return inner(diff)
