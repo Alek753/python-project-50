6447c52863c641c56c23cfb3cb1badc8033e24fb
Aleksei Semenov <alek753@yandex.ru>
2024-02-02 16:22:59 +0400

    'plain' is working

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,6 +1,30 @@
-def inner(data):
+def make_string(current_data):
+    if isinstance(current_data, dict):
+        return '[complex value]'
+    if isinstance(current_data, bool):
+        return str(current_data).lower()
+    if current_data is None:
+        return 'null'
+    return f"'{str(current_data)}'"
+
+
+def inner(data, node=''):
     result = []
-    return result
+    for key, value in data.items():
+        node_name = f"{node}{value['key']}"
+        property_string = f"Property '{node_name}'"
+        if value['operation'] == 'removed':
+            result.append(f"{property_string} was removed")
+        if value['operation'] == 'added':
+            result.append(f"{property_string} was added "
+                          f"with value: {make_string(value['value'])}")
+        if value['operation'] == 'changed':
+            result.append(f"{property_string} was updated. "
+                          f"From {make_string(value['prev'])} "
+                          f"to {make_string(value['new'])}")
+        if value['operation'] == 'nested':
+            result.append(inner(value['value'], f"{node_name}."))
+    return '\n'.join(result)
 
 
 def make_plain(diff):
