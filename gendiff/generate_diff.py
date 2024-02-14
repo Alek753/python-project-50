443c24c4dcb7c95b0e8004b6ad9f9f82b71f2345
Aleksei Semenov <alek753@yandex.ru>
2024-02-14 10:29:19 +0400

    fixes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,8 +6,8 @@ from gendiff.formatters.choose_formatter import choose_formatter
 def generate_diff(file1, file2, formatter='stylish'):
     data1 = read_data(file1)
     data2 = read_data(file2)
-    print(data1)
-    print(data2)
+#    print(data1)
+#    print(data2)
     if data1 is None:
         raise ValueError(f'File {file1} is empty!')
     if data2 is None:
