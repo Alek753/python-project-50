e77f691e68e16bd49eaee09fac02ce813c45605a
Aleksei Semenov <alek753@yandex.ru>
2024-02-14 11:46:35 +0400

    clearing some unused code in 'generate_diff'

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,8 +6,4 @@ from gendiff.formatters.choose_formatter import choose_formatter
 def generate_diff(file1, file2, formatter='stylish'):
     data1 = read_data(file1)
     data2 = read_data(file2)
-    if data1 is None:
-        raise ValueError(f'File {file1} is empty!')
-    if data2 is None:
-        raise ValueError(f'File {file2} is empty!')
     return choose_formatter(make_diff(data1, data2), formatter)
